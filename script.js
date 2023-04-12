0727d9d07454ee83682c34fa0c143ed826a99001
Shubhamjindal0801 <52204183+Shubhamjindal0801@users.noreply.github.com>
2023-04-12 15:42:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-//complete this code
-class Animal {
-	constructor(species){
-		this.species = species
-	}
-	get species(){
-		return this.species
-	}
-	makeSound(){
-		console.log(`The ${species} makes a sound`);
-	}
+class Animal{
+    constructor(species){
+        this.species=species;
+    }
+    // get species(){
+    //     return this.species;
+    // }
+    makeSound(){
+        console.log(this.species+" animal makes sound");
+    }
 }
 
-class Dog extends Animal {
-	bark(){
-		console.log("Woof")
-	}
+class Dog extends Animal{
+    constructor(species){
+        super(species);
+    }
+    bark(){
+        console.log("Woof");
+    }
 }
 
-class Cat extends Animal {
-	purr(){
-		console.log("purr")
-	}
+class Cat extends Animal{
+    constructor(species){
+        super(species);
+    }
+    purr(){
+        console.log("purr");
+    }
 }
-const cat = new Cat("Kitty")
-cat.makeSound();
-cat.purr();
-const dog = new Dog("Ligar")
-dog.makeSound();
-dog.bark();
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+
+var cat = new Cat("Kitty");
+console.log(cat.makeSound())
+console.log(cat.purr())
+var dog = new Dog("Ligar");
+console.log(dog.makeSound())
+console.log(dog.bark())
